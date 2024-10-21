365ec2e36f19aa13015d94c45e6df2d258ac0945
KalebR6101 <143465678+KalebR6101@users.noreply.github.com>
2024-10-21 14:13:17 -0500

    u

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String name;
+    int age;
+    int grade;
+    int coursegrade;
+
+    public Student(String name, int age, int grade, int coursegrade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.coursegrade = coursegrade;
+    }
+
+}
